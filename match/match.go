d3401ffd1401def907efc582c066900509d5cc3b
Jérémie Lawson <jl@qrs.fr>
2019-02-07 11:03:30 +0100

    Match functions.

--- match/match.go
+++ match/match.go
@@ -4,29 +4,54 @@ import (
 	"math"
 
 	"github.com/cpmech/gosl/graph"
+	uuid "github.com/satori/go.uuid"
 )
 
-type Mate struct {
-	value float64
+type Genre uint
+
+const (
+	Male Genre = iota
+	Female
+	Unspecified
+)
+
+type Patient struct {
+	id     uuid.UUID
+	age    int
+	genre  Genre
+	points uint64
 }
 
-func (mate *Mate) Preference(other *Mate) float64 {
-	if mate == other {
+// calcule la proximité d'un patient avec un autre selon des critères définis.
+// TODO: à ameliorer pour un meilleur matching
+func patientProximity(a, b *Patient) float64 {
+	if a.id == b.id {
+		// TODO: don't match with yourself
 		return math.MaxFloat64
 	}
-	return math.Abs(mate.value - other.value)
+	ageDiff := math.Abs(float64(a.age) - float64(b.age))
+	pointsDiff := math.Abs(float64(a.points) - float64(b.points))
+
+	var difference float64
+	if a.genre != b.genre {
+		// valeur prise au pif
+		difference += 20
+	}
+
+	return pointsDiff + ageDiff + difference
 }
 
-func Match(mates []*Mate) [][2]*Mate {
+func matchPatients(patients []*Patient, compare func(a, b *Patient) float64) [][2]*Patient {
 	var mnk graph.Munkres
-	length := len(mates)
+
+	length := len(patients)
 	mnk.Init(length, length)
 
 	matrix := make([][]float64, 0, length)
-	for _, mateX := range mates {
+	for _, patientX := range patients {
 		row := make([]float64, length)
-		for y, mateY := range mates {
-			row[y] = mateX.Preference(mateY)
+		for y, patientY := range patients {
+			row[y] = compare(patientX, patientY)
 		}
 		matrix = append(matrix, row)
 	}
@@ -34,10 +59,14 @@ func Match(mates []*Mate) [][2]*Mate {
 	mnk.SetCostMatrix(matrix)
 	mnk.Run()
 
-	res := make([][2]*Mate, len(mnk.Links))
+	res := make([][2]*Patient, len(mnk.Links))
 	for x, y := range mnk.Links {
-		res[x] = [2]*Mate{mates[x], mates[y]}
+		res[x] = [2]*Patient{patients[x], patients[y]}
 	}
 
 	return res
+}
+
+func MatchPatients(patients []*Patient) [][2]*Patient {
+	return matchPatients(patients, patientProximity)
 }
--- match/match_test.go
+++ match/match_test.go
@@ -2,23 +2,48 @@ package match
 
 import (
 	"fmt"
+	"math"
 	"testing"
+
+	uuid "github.com/satori/go.uuid"
 )
 
 func TestMatch(t *testing.T) {
-	list := []*Mate{
-		&Mate{4},
-		&Mate{9},
-		&Mate{34},
-		&Mate{124},
-		&Mate{13},
-		&Mate{7},
-		&Mate{213},
-		&Mate{56},
+	list := []*Patient{
+		&Patient{id: uuid.NewV4(), genre: Male, age: 18},
+		&Patient{id: uuid.NewV4(), genre: Female, age: 27},
+		&Patient{id: uuid.NewV4(), genre: Female, age: 34},
+		&Patient{id: uuid.NewV4(), genre: Male, age: 79},
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 21},
+		&Patient{id: uuid.NewV4(), genre: Female, age: 40},
+		&Patient{id: uuid.NewV4(), genre: Male, age: 30},
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 56},
 	}
-	res := Match(list)
+	res := MatchPatients(list)
 
 	for _, couple := range res {
-		fmt.Println(couple[0].value, couple[1].value)
+		fmt.Println(couple[0], couple[1])
+	}
+}
+
+func TestMatchAge(t *testing.T) {
+	list := []*Patient{
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 18},
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 41},
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 34},
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 35},
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 57},
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 40},
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 19},
+		&Patient{id: uuid.NewV4(), genre: Unspecified, age: 56},
+	}
+
+	res := MatchPatients(list)
+
+	for _, couple := range res {
+		fmt.Println(couple[0], couple[1])
+		if math.Abs(float64(couple[0].age-couple[1].age)) > 1 {
+			t.Fail()
+		}
 	}
 }
